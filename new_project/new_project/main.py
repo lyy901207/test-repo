024b65c1eb6ff5e7d944ea71f0b52853ea767c27
yanyan <lyy901207@gmail.com>
2024-04-10 23:24:30 +0900

    update test

--- new_project/new_project/main.py
+++ new_project/new_project/main.py
@@ -1,26 +1,12 @@
-def binary_search(arr, target):
-    low = 0
-    high = len(arr) - 1
+"""
+This is the main module of my program. It contains a function that returns a list of integers.
+"""
 
-    while low <= high:
-        mid = (low + high) // 2
-        if arr[mid] == target:
-            return mid
-        elif arr[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-
-    return -1
+from typing import List
 
 
-# Test case
-if __name__ == "__main__":
-    arr = [2, 4, 6, 8, 10]
-    target = 8
-    result = binary_search(arr, target)
-    print(
-        f"Target {target} found at index {result}"
-        if result != -1
-        else "Target not found"
-    )
+def test() -> List[int]:
+    """
+    This function returns a list of integers.
+    """
+    return [1, 2, 3]
